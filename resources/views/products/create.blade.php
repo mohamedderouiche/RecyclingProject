272a93be5f7f2ad1eb43115e506c3a43f605ab0c
Sonia-Chalouah <soniachalouah1@gmail.com>
2024-09-30 17:36:24 +0100

    product

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -9,20 +9,17 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Green Recycle</title>
+    <title>Green Recycle - Dashboard</title>
     <link rel="icon" href="{{ asset('img/logo/recycling.ico') }}" type="image/x-icon">
+    
     <!-- Custom fonts for this template-->
     <link href="{{ asset('admin/vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
     <!-- Custom styles for this template-->
     <link href="{{ asset('admin/css/sb-admin-2.min.css') }}" rel="stylesheet">
     
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
-
     
-   
-
 </head>
-
 
 <body id="page-top">
 
@@ -35,85 +32,104 @@
 
         <!-- Content Wrapper -->
         <div id="content-wrapper" class="d-flex flex-column">
-
+            <!-- Main Content -->
             <div id="content">
-
 
                 <!-- Topbar -->
                 <nav class="navbar navbar-expand navbar-dark bg-light topbar mb-4 static-top shadow">
-
                     <!-- Sidebar Toggle (Topbar) -->
                     <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
-                        <i class="fa fa-bars">zaazaz</i>
+                        <i class="fa fa-bars"></i>
                     </button>
-
                     <!-- Topbar Search -->
-                   @include('admin.topbar')
+                    @include('admin.topbar')
+                </nav>
                 <!-- End of Topbar -->
 
-           <div>
-<div class="container">
-    <h1 class="my-4">Create Product</h1>
+                <!-- Page Content -->
+                <div class="container mt-5">
+                    <h1 class="mb-4 text-center">Create New Product</h1>
+                    <div class="row justify-content-center"> <!-- Center the form -->
+                        <div class="col-md-6"> <!-- Set the width of the form -->
+                            <form action="{{ route('products.store') }}" method="POST" enctype="multipart/form-data">
+                                @csrf
+                                <!-- Product Name -->
+                                <div class="form-group">
+                                    <label for="name">Product Name</label>
+                                    <input type="text" class="form-control" id="name" name="name" placeholder="Enter product name" required>
+                                </div>
 
-    <form action="{{ route('products.store') }}" method="POST" enctype="multipart/form-data">
-        @csrf
-        <div class="mb-3">
-            <label for="name" class="form-label">Name</label>
-            <input type="text" name="name" id="name" class="form-control" required>
+                                <!-- Product Description -->
+                                <div class="form-group">
+                                    <label for="description">Description</label>
+                                    <textarea class="form-control" id="description" name="description" rows="2" placeholder="Enter product description" required></textarea>
+                                </div>
+
+                                <!-- Product Price -->
+                                <div class="form-group">
+                                    <label for="price">Price</label>
+                                    <input type="text" class="form-control" id="price" name="price" placeholder="Enter product price" required>
+                                </div>
+
+                                <!-- Image Upload -->
+                                <div class="form-group">
+                                    <label for="image">Product Image</label>
+                                    <input type="file" class="form-control" id="image" name="image" required>
+                                </div>
+
+                                <!-- Category Selection -->
+                                <div class="form-group">
+                                    <label for="categories_id">Category</label>
+                                    <select name="categories_id" id="categories_id" class="form-control" required>
+                                        @foreach ($categories as $category)
+                                            <option value="{{ $category->id }}">{{ $category->name }}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+
+                                <!-- Submit Button -->
+                                <button type="submit" class="btn btn-primary btn-block">Create Product</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            <!-- End of Main Content -->
+
+            <!-- Footer -->
+            @include('admin.footer')
+            <!-- End of Footer -->
         </div>
-        <div class="mb-3">
-            <label for="description" class="form-label">Description</label>
-            <textarea name="description" id="description" class="form-control" rows="3" required></textarea>
-        </div>
-        <div class="mb-3">
-            <label for="price" class="form-label">Price</label>
-            <input type="text" name="price" id="price" class="form-control" required>
-        </div>
-        <div class="mb-3">
-            <label for="image" class="form-label">Image</label>
-            <input type="file" name="image" id="image" class="form-control" required>
-        </div>
-        <div class="mb-3">
-            <label for="categories_id" class="form-label">Category</label>
-            <select name="categories_id" id="categories_id" class="form-control" required>
-                @foreach ($categories as $category)
-                    <option value="{{ $category->id }}">{{ $category->name }}</option>
-                @endforeach
-            </select>
-        </div>
-        <button type="submit" class="btn btn-primary">Create</button>
-    </form>
-</div>
-</div>
-<!-- End of Content Wrapper -->
+        <!-- End of Content Wrapper -->
+    </div>
+    <!-- End of Page Wrapper -->
 
-</div>
-<!-- End of Page Wrapper -->
+    <!-- Scroll to Top Button-->
+    <a class="scroll-to-top rounded" href="#page-top">
+        <i class="fas fa-angle-up"></i>
+    </a>
 
-<!-- Scroll to Top Button-->
-<a class="scroll-to-top rounded" href="#page-top">
-<i class="fas fa-angle-up"></i>
-</a>
+    <!-- Logout Modal-->
+    @include('admin.logout')
 
-<!-- Logout Modal-->
-@include('admin.logout')
-<!-- Bootstrap core JavaScript-->
-<script src="admin/vendor/jquery/jquery.min.js"></script>
-<script src="admin/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <!-- Bootstrap core JavaScript-->
+    <script src="{{ asset('admin/vendor/jquery/jquery.min.js') }}"></script>
+    <script src="{{ asset('admin/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
 
-<!-- Core plugin JavaScript-->
-<script src="admin/vendor/jquery-easing/jquery.easing.min.js"></script>
+    <!-- Core plugin JavaScript-->
+    <script src="{{ asset('admin/vendor/jquery-easing/jquery.easing.min.js') }}"></script>
 
-<!-- Custom scripts for all pages-->
-<script src="admin/js/sb-admin-2.min.js"></script>
+    <!-- Custom scripts for all pages-->
+    <script src="{{ asset('admin/js/sb-admin-2.min.js') }}"></script>
 
-<!-- Page level plugins -->
-<script src="admin/vendor/chart.js/Chart.min.js"></script>
+    <!-- Page level plugins -->
+    <script src="{{ asset('admin/vendor/chart.js/Chart.min.js') }}"></script>
 
-<!-- Page level custom scripts -->
-<script src="admin/js/demo/chart-area-demo.js"></script>
-<script src="admin/js/demo/chart-pie-demo.js"></script>
+    <!-- Page level custom scripts -->
+    <script src="{{ asset('admin/js/demo/chart-area-demo.js') }}"></script>
+    <script src="{{ asset('admin/js/demo/chart-pie-demo.js') }}"></script>
 
 </body>
-</html>
 
+</html>
