58c45d7e92586eaadd2046fd78da09e8a8393df3
roua mnissi <73589387+rouamn@users.noreply.github.com>
2024-10-19 16:34:15 +0200

    update

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,6 +20,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->enum('role', ['admin', 'user'])->default('user'); // Add role attribute with 'user' as the default
             $table->timestamps();
         });
     }
